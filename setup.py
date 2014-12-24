84a9042919052c5ac4d168fe228fad5e97e95979
Jeff Edwards <jeff@edwardsj.com>
2014-12-24 19:19:28 +0000

    Updates to configuration so builds work

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1',
     description='Configuration-based OO-dependency resolution workflow engine',
     author='Jeff Edwards',
-    author_email='jeff@edwardsj.com'
+    author_email='jeff@edwardsj.com',
     url='https://github.com/python-daisy/daisy',
     license='MIT License',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
